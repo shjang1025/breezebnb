040ec9557e237864ada919b309d269dad82ada95
shjang1025 <shjang1025@gmail.com>
2024-04-04 10:13:11 -0700

    edit hosting page styling

--- frontend/src/components/HostForm/addRoomForm.jsx
+++ frontend/src/components/HostForm/addRoomForm.jsx
@@ -19,16 +19,18 @@ const AddRoomForm = props => {
     const [rooms, setRooms] = useState(0)
     const [baths, setBaths] = useState(0)
     const [parking, setParking] = useState(false)
-    const [washer, setwasher] = useState(false)
+    const [washer, setWasher] = useState(false)
     const [dryer, setDryer] = useState(false)
     const [tv, setTv] = useState(false)
     const [ac, setAC] = useState(false)
     const [heater, setHeater] = useState(false)
+    const [wifi, setWifi] = useState(false)
     const [kitchen, setKitchen] = useState(false)
     const [microwave, setMicrowave] = useState(false)
     const [fireplace, setFireplace] = useState(false)
     const [pets, setPets] = useState(false)
 
+    
     return(
         <>
             <Navbar/>
@@ -100,56 +102,119 @@ const AddRoomForm = props => {
                                 placeholder="Capacity"  />
                         </div>
                     </label>
-
-                    <div className="yn-container">
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faSquareParking} size="xl"/> 
-                            Parking
-                            <input type="checkbox" name="has_parking" />
+                    <label id="num-beds"> How many bedrooms/beds/bathrooms ?</label>
+                    <div className="bed-container">
+                        <label >Rooms: 
+                            <span>
+                                <input type="number" min="0" value={rooms} onChange={e => setRooms(e.target.value)}/>
+                            </span>
                         </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faShirt} size="xl"/>  Washer
-                            <input type="checkbox" name="has_washer" />
+                        <label >Beds: 
+                            <span>
+                                <input type="number" min="0" value={beds} onChange={e => setBeds(e.target.value)}/>
+                            </span>
                         </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faSocks} size="xl"/>  Dryer
-                            <input type="checkbox" name="has_dryer" />
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faTv} size="xl"/>  TV
-                            <input type="checkbox" name="has_tv" />
-                            
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faIgloo} size="xl"/>  AC
-                            <input type="checkbox" name="has_AC" />
-                            
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faTemperatureArrowUp} size="xl"/>  Heater
-                            <input type="checkbox" name="has_heater" />
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faWifi} size="xl"/>  Wi-Fi
-                            <input type="checkbox" name="has_wifi" />
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faSink} size="xl"/>  Kitchen
-                            <input type="checkbox" name="has_kitchen" />
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faFireBurner} size="xl"/>  Microwave
-                            <input type="checkbox" name="has_microwave" />
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faFire} size="xl"/>  Fire Place
-                            <input type="checkbox" name="has_fireplace" />
-                        </label>
-                        <label className="yn">
-                            <FontAwesomeIcon icon={faDog} size="xl"/>  Pet
-                            <input type="checkbox" name="has_pet" />
+                        <label >Bathrooms: 
+                            <span>
+                                <input type="number" min="0" value={baths} onChange={e => setBaths(e.target.value)}/>
+                            </span>
                         </label>
                     </div>
+                    {/* boolean type lists */}
+                    <div className="yn-container">
+                        <label>Please choose the provided facilities</label>
+                        <label className="yn">
+                            <span>
+                            <FontAwesomeIcon icon={faSquareParking} size="xl"/> Parking
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_parking" value={parking} onChange={e => setParking(e.target.value)}/>
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faShirt} size="xl" />  Washer
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_washer" value={washer} onChange={e => setWasher(e.target.value)}/>
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faSocks} size="xl" />  Dryer
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_dryer" value={dryer} onChange={e => setDryer(e.target.value)}/>
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faTv} size="xl"/>  TV
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_tv" value={tv} onChange={e => setTv(e.target.value)} />
+                            </span>
+                            
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faIgloo} size="xl"/>  AC
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_AC" value={ac} onChange={e => setAC(e.target.value)}/>
+                            </span>
+                            
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faTemperatureArrowUp} size="xl"/>  Heater
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_heater" value={heater} onChange={e => setHeater(e.target.value)}/>
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faWifi} size="xl"/>  Wi-Fi
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_wifi" value={wifi} onChange={e => setWifi(e.target.value)}/>
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faSink} size="xl"/>  Kitchen
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_kitchen" value={kitchen} onChange={e => setKitchen(e.target.value)}/>
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faFireBurner} size="xl"/>  Microwave
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_microwave" value={microwave} onChange={e => setMicrowave(e.target.value)} />
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faFire} size="xl"/>  Fire Place
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_fireplace" value={fireplace} onChange={e => setFireplace(e.target.value)}/>
+                            </span>
+                        </label>
+                        <label className="yn">
+                            <span>
+                                <FontAwesomeIcon icon={faDog} size="xl"/>  Pet
+                            </span>
+                            <span>
+                                <input type="checkbox" name="has_pet" value={pets} onChange={e => setPets(e.target.value)} />
+                            </span>
+                        </label>
+                    </div>
+                    
 
                 </form>
             </div>
