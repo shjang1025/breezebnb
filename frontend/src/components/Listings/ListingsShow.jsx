7c7ae7cd8fe8ae69c88f058256f83e9ddda23b1e
shjang1025 <shjang1025@gmail.com>
2024-04-08 17:59:00 -0700

    make reservation form in listing show

--- frontend/src/components/Listings/ListingsShow.jsx
+++ frontend/src/components/Listings/ListingsShow.jsx
@@ -3,13 +3,19 @@ import Navbar from "../Navbar"
 import "./ListingsShow.css"
 import { useState, useEffect } from "react"
 import {faSquareParking, faTv, faIgloo, faTemperatureArrowUp,faChevronLeft, faChevronRight,
-    faShirt, faSocks, faWifi, faSink, faFireBurner,faFire,faDog} from "@fortawesome/free-solid-svg-icons"
+    faShirt, faSocks, faWifi, faSink, faFireBurner,faFire,faDog, faAngleDown} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { differenceInDays } from "date-fns"; 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const ListingsShow = () => {
     const {room_id} = useParams();
     const [selectedRoom, setSelectedRoom] = useState(null);
     const [username, setUsername] = useState(null);
+    const [checkInDate, setCheckInDate] = useState(null);
+    const [checkOutDate, setCheckOutDate] = useState(null);
 
     useEffect(() => {
         fetchRoomData(room_id)
@@ -42,6 +48,28 @@ const ListingsShow = () => {
             console.error('Error fetching user data:', error);
         }
     }
+    const daysCalculation = () => {
+        if (checkInDate && checkOutDate) {
+          return differenceInDays(checkOutDate, checkInDate);
+        }
+        return 0; // Default to 0 if either date is not set
+    };
+    const cleaningFeeRange = () => {
+        if(selectedRoom.price <= 100) {
+            return 20
+        } else if (selectedRoom.price <= 170) {
+            return 53
+        } else if (selectedRoom.price <= 260) {
+            return 72
+        } else if (selectedRoom.price <= 400) {
+            return 100
+        } else if (selectedRoom.price <= 600) {
+            return 120
+        } else {
+            return 150
+        }
+    }
+
     return(
         <>
             <Navbar/>
@@ -148,7 +176,112 @@ const ListingsShow = () => {
 
                             </div>
                             <div className="details-right">
-                                <p>Reservation to be continued...</p>
+                                <div className="reservation-outer-container">
+                                    <div className="reservation-container">
+                                        <div className="price-per-night">
+                                            <p><span className="bold-text">${selectedRoom.price}</span> night</p>
+                                        </div>
+                                        <button className="reservation-date-button">
+                                            <div className="left">
+                                                <div className="checkin">
+                                                    Check-in
+                                                </div>
+                                                <div className="checkin-input">
+                                                    <DatePicker
+                                                        selected={checkInDate}
+                                                        onChange={(date) => setCheckInDate(date)}
+                                                        selectsStart
+                                                        startDate={checkInDate}
+                                                        endDate={checkOutDate}
+                                                        minDate={new Date()} // Disable past dates
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="right">
+                                                <div className="checkout">
+                                                    Check-out
+                                                </div>
+                                                <div className="checkout-input">
+                                                <DatePicker
+                                                    selected={checkOutDate}
+                                                    onChange={(date) => setCheckOutDate(date)}
+                                                    selectsEnd
+                                                    startDate={checkInDate}
+                                                    endDate={checkOutDate}
+                                                    minDate={checkInDate || new Date()} // Disable past dates and dates before check-in
+                                                />
+                                                </div>
+                                            </div>
+                                        </button>
+                                        <div className="guest-dropdown">
+                                            <label className="guest-picker"> 
+                                                <div className="guest-text">Guests</div>
+                                                <div className="guest-select">
+                                                    <span>2 guests</span>
+                                                </div>
+                                            </label>
+                                            <div className="guest-dropdown-arrow">
+                                                <FontAwesomeIcon icon={faAngleDown}/>
+                                            </div>
+                                        </div>
+
+    
+                                    </div>
+                                    <div className="reservation-button-container">
+                                            <div className="button-container">
+                                                <button>Reserve</button>
+                                            </div>
+                                            <div>
+                                                You won't be charged yet
+                                            </div>
+                                    </div>
+                                    <div className="reservation-fee-calculation">
+                                        <div className="reservation-fee-upper-container">
+                                            <div className="reservation-fee-left">
+                                                <span className="reservation-text">
+                                                    ${selectedRoom.price} x {daysCalculation()} nigths
+                                                </span>
+                                                <span className="reservation-text">
+                                                    Cleaning fee
+                                                </span>
+                                                <span className="service-fee-text">
+                                                    Breezebnb Service fee
+                                                </span>
+                                                
+                                            </div>
+                                            <div className="reservation-fee-right">
+                                                <span className="reservation-text">
+                                                    ${selectedRoom.price * daysCalculation()}
+                                                </span>
+                                                <span className="reservation-text">
+                                                    ${cleaningFeeRange()}
+                                                </span>
+                                                <span className="reservation-text">
+                                                    ${30}
+                                                </span>
+                                                
+                                            </div>
+                                        </div>
+                                        <div className="divider"></div>
+                                        <div className="reservation-fee-below-cotainer">
+                                            <div className="total">
+                                                <div className="total-left">
+                                                    <span className="reservation-text">
+                                                        Total before Taxes
+                                                    </span>
+                                                </div>
+                                                <div className="total-right">
+                                                    <span className="reservation-text">
+                                                        ${daysCalculation() * selectedRoom.price + cleaningFeeRange() + 30}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                        
+                                    </div>
+                                    
+                                </div>
                             </div>
                         </div>
 
